718dc9febbf5622062988e0b5aa49331a2965fbc
unknown <khandanamir7@gmail.com>
2025-01-05 21:09:00 +0330

    Event file created.

--- lib/posts/bloc/post_event.dart
+++ lib/posts/bloc/post_event.dart
@@ -1,1 +1,10 @@
+part of 'post_bloc.dart';
 
+/// Base event class
+sealed class PostEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+/// [PostFetched] event which informs the [PostBloc] that the posts are fetched
+final class PostFetched extends PostEvent {}
