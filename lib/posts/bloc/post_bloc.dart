b9a696b4b3c905cf7a2baec38e1fbd7d37626d38
unknown <khandanamir7@gmail.com>
2025-01-05 21:18:26 +0330

    Created bloc states

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:infinite_list/posts/models/post.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
--- lib/posts/bloc/post_state.dart
+++ lib/posts/bloc/post_state.dart
@@ -1,1 +1,37 @@
 part of 'post_bloc.dart';
+
+enum PostStatus { inital, success, failure }
+
+final class PostState extends Equatable {
+  final PostStatus status;
+  final List<Post> posts;
+  final bool hasReachedMax;
+
+  const PostState({
+    this.status = PostStatus.inital,
+    this.posts = const <Post>[],
+    this.hasReachedMax = false,
+  });
+
+// Implemented ```copyWith``` so that we can copy an instance of PostSuccess
+// and update zero or more properties conveniently (this will come in handy later).
+  PostState copyWith({
+    PostStatus? status,
+    List<Post>? posts,
+    bool? hasReachedMax,
+  }) {
+    return PostState(
+      status: status ?? this.status,
+      posts: posts ?? this.posts,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+    );
+  }
+
+  @override
+  String toString() {
+    return '''PostState { status: $status, hasReachedMax: $hasReachedMax, posts: ${posts.length} }''';
+  }
+
+  @override
+  List<Object?> get props => [status, posts, hasReachedMax];
+}
