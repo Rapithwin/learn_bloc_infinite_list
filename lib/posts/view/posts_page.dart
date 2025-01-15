5efa899ee1b692384e49d00ee4093d5e13e61632
unknown <khandanamir7@gmail.com>
2025-01-15 17:01:28 +0330

    BlocProvider added to PostPage

--- lib/posts/view/posts_page.dart
+++ lib/posts/view/posts_page.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_list/posts/bloc/post_bloc.dart';
+import 'package:infinite_list/posts/posts_api/posts_api.dart';
+import 'package:infinite_list/posts/posts_api/posts_repository.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:infinite_list/posts/view/posts_list.dart';
 
 class PostsPage extends StatelessWidget {
   const PostsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    PostsRepository repo =
+        PostsRepository(postsApi: PostsApi(httpClient: http.Client()));
+    return Scaffold(
+      body: BlocProvider(
+        create: (_) => PostBloc(repo),
+        child: const PostsList(),
+      ),
+    );
   }
 }
