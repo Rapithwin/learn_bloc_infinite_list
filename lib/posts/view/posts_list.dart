324201a8e841209d1b8970e6e3ee8a6c07cc22f2
unknown <khandanamir7@gmail.com>
2025-01-15 17:22:29 +0330

    scrollController implemented

--- lib/posts/view/posts_list.dart
+++ lib/posts/view/posts_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:infinite_list/posts/bloc/post_bloc.dart';
 
 class PostsList extends StatefulWidget {
   const PostsList({super.key});
@@ -8,6 +10,31 @@ class PostsList extends StatefulWidget {
 }
 
 class _PostsListState extends State<PostsList> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    _scrollController.addListener(_onScroll);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom) context.read<PostBloc>().add(PostFetched());
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
