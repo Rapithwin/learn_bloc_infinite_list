418a3704b909f19847612c72440cf17f1753a956
unknown <khandanamir7@gmail.com>
2025-01-15 13:46:10 +0330

    view directory created

---
+++ lib/posts/view/posts_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostsList extends StatefulWidget {
+  const PostsList({super.key});
+
+  @override
+  State<PostsList> createState() => _PostsListState();
+}
+
+class _PostsListState extends State<PostsList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
