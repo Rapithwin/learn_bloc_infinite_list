8fcdacc991a58a9ace4aeee7ea448fa9a55328d2
unknown <khandanamir7@gmail.com>
2025-01-16 22:01:05 +0330

    Added a simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_list/app.dart';
+import 'package:infinite_list/simple_bloc_observer.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(const App());
 }
